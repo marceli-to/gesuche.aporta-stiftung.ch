f6239d8fd4cdb2adebcd5abd4c6bcee47297071b
Marcel Stadelmann <m@marceli.to>
2022-12-13 13:17:07 +0100

    staging build

--- resources/views/mails/confirmation.blade.php
+++ resources/views/mails/confirmation.blade.php
@@ -1,6 +1,11 @@
 @component('mail::message')
 <h1>Bestätigung Beitragsgesuch</h1>
-<p>Sehr geehrte {{ $application->firstname }} {{ $application->lastname }}</p>
-<p>Wir haben Ihre Daten erhalten und werden diese in der kommenden Zeit prüfen.</p>
-<p>Freundliche Grüsse<br><br>{{ env('APP_NAME') }}</p>
+<p>Sehr geehrte/r {{ $application->firstname }} {{ $application->lastname }}</p>
+<p>Wir haben Ihr Gesuch heute erhalten. Besten Dank. Über die Verteilung des Reinertrages unseres Stiftungsvermögens wird im nächsten Sommer entschieden. Sie werden in jedem Fall eine Nachricht erhalten.</p>
+<p>
+Freundliche Grüsse<br>
+<div>
+  <img src="https://gesuche.aporta-stiftung.ch/assets/img/logo.png" height="43" width="100" style="display: block; height: 43px; width: 100px">
+</div>
+</p>
 @endcomponent
--- resources/views/mails/notification.blade.php
+++ resources/views/mails/notification.blade.php
@@ -1,5 +1,3 @@
 @component('mail::message')
-<h1>Eingang Beitragsgesuch</h1>
-<p>{{ $application->firstname }} {{ $application->lastname }} ({{ $application->name }}) hat ein neues Gesuch eingereicht.
-<p>Freundliche Grüsse<br><br>{{ env('APP_NAME') }}</p>
+<p>Eingang Beitragsgesuch {{ $application->name }}.</p>
 @endcomponent
