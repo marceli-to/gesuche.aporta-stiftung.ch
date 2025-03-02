658e9c53fdcdb6ab7624425cc93082ba8064e386
Marcel Stadelmann <m@marceli.to>
2025-03-02 08:32:28 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@ use App\Http\Controllers\FormController;
 |
 */
 
-Route::get('/formular', [FormController::class, 'index']);
-Route::post('/form/submit', [FormController::class, 'store']);
+// Route::get('/formular', [FormController::class, 'index']);
+// Route::post('/form/submit', [FormController::class, 'store']);
+
 Route::post('/file/upload', [FormController::class, 'upload']);
 Route::delete('/file/upload/{filename}', [FormController::class, 'delete']);
-
-// Route::get('/formular', [PageController::class, 'index']);
+Route::get('/formular', [PageController::class, 'index']);
 
 // Auth routes
 Auth::routes(['verify' => true, 'register' => false]);
