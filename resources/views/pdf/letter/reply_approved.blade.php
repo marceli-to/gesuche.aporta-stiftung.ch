584566d969249c73958678e09effa1ad2deb4a7f
Marcel Stadelmann <m@marceli.to>
2025-05-28 08:37:55 +0200

    wip

--- resources/views/pdf/letter/reply_approved.blade.php
+++ resources/views/pdf/letter/reply_approved.blade.php
@@ -3,20 +3,20 @@
 <div class="content @if (!$loop->last) break @endif">
 <div>{{ $d->name }}<br>{{ $d->firstname }} {{ $d->lastname }}<br>{{ $d->street }} {{ $d->street_number ?? $d->street_number }}<br>{{ $d->zip }} {{ $d->city }}</div>
 <br><br><br><br><br>
-<div>Zürich, {{ strftime('%e. %B %Y', time()) }} AI/pc</div>
+<div>Zürich, 10. Juli 2025 AI/pc</div>
 <br><br><br>
-<div><strong>Vergabung aus unserem Reinertrag pro 2023<br>Ihr Beitragsgesuch vom {{ $d->created_at_formated }}</strong></div>
+<div><strong>Vergabung aus unserem Reinertrag pro 2024<br>Ihr Beitragsgesuch vom {{ $d->created_at_formated }}</strong></div>
 <br><br>
 <div>{!! $d->salutation ? nl2br($d->salutation)  : 'Sehr geehrte Damen und Herren' !!}</div>
 <br>
-<div>Wir freuen uns, Ihnen mitteilen zu können, dass der Stiftungsrat anlässlich seiner Sitzung vom 18. Juni 2024 gemäss Vorschlag des Stadtrates von Zürich beschlossen hat, Ihrer Institution aus dem Reinertrag unseres Stiftungsvermögens den Betrag von CHF {{ AppHelper::number($d->project_contribution_approved) }}, als Beitrag an {{ $d->textblock_approval }}, zukommen zu lassen.<br><br>@if ($d->textblock_justification) {{ $d->textblock_justification }} @endif Der Beitrag muss zweckgebunden verwendet werden.</div>
+<div>Wir freuen uns, Ihnen mitteilen zu können, dass der Stiftungsrat anlässlich seiner Sitzung vom 17. Juni 2025 gemäss Vorschlag des Stadtrates von Zürich beschlossen hat, Ihrer Institution aus dem Reinertrag unseres Stiftungsvermögens den Betrag von CHF {{ AppHelper::number($d->project_contribution_approved) }}, als Beitrag an {{ $d->textblock_approval }}, zukommen zu lassen.<br><br>@if ($d->textblock_justification) {{ $d->textblock_justification }} @endif Der Beitrag muss zweckgebunden verwendet werden.</div>
 <br>
-<div>Wir bitten Sie, uns den Eingang des erwähnten Betrages, welchen wir Ihnen bis Ende Juli 2024 auf Ihr Konto mit IBAN {{ $d->iban_formated ? $d->iban_formated :  $d->bank_account }} überweisen werden, umgehend schriftlich zu bestätigen.</div>
+<div>Wir bitten Sie, uns den Eingang des erwähnten Betrages, welchen wir Ihnen bis Ende Juli 2025 auf Ihr Konto mit IBAN {{ $d->iban_formated ? $d->iban_formated :  $d->bank_account }} überweisen werden, umgehend schriftlich zu bestätigen.</div>
 <br>
 <div>Weiter sind wir Ihnen dankbar, wenn Sie, dem Wunsch des Stifters entsprechend, im Gönnerverzeichnis Ihres Jahresberichtes vermerken würden, dass es sich um eine Zuwendung unserer Stiftung handelt.</div>
 <br>
 @if ($d->project_contribution_approved >= '20000')
-<div>Bitte reichen Sie uns bis spätestens Ende März 2025 Ihren Schlussbericht ein. Genauere Informationen entnehmen Sie bitte der Beilage.</div>
+<div>Bitte reichen Sie uns bis spätestens Ende März 2026 Ihren Schlussbericht ein. Genauere Informationen entnehmen Sie bitte der Beilage.</div>
 <br>
 @endif
 <div>Freundliche Grüsse</div>
--- resources/views/pdf/letter/reply_denied.blade.php
+++ resources/views/pdf/letter/reply_denied.blade.php
@@ -3,7 +3,7 @@
 <div class="content @if (!$loop->last) break @endif">
 <div>{{ $d->name }}<br>{{ $d->firstname }} {{ $d->lastname }}<br>{{ $d->street }} {{ $d->street_number ?? $d->street_number }}<br>{{ $d->zip }} {{ $d->city }}</div>
 <br><br><br><br><br>
-<div>Zürich, {{ strftime('%e. %B %Y', time()) }} AI/pc</div>
+<div>Zürich, 10. Juli 2025 AI/pc</div>
 <br><br><br>
 <div><strong>Gesuch um finanzielle Unterstützung</strong></div>
 <br><br>
@@ -11,7 +11,7 @@
 <br>
 <div>Wir nehmen Bezug auf Ihr Gesuch vom {{ $d->created_at_formated }}.</div>
 <br>
-<div>Unser Stiftungsrat hat in seiner Sitzung vom 18. Juni 2024 über die Zuwendungen aus dem Reinertrag des Stiftungsvermögens pro 2023 Beschluss gefasst.</div>
+<div>Unser Stiftungsrat hat in seiner Sitzung vom 17. Juni 2025 über die Zuwendungen aus dem Reinertrag des Stiftungsvermögens pro 2024 Beschluss gefasst.</div>
 <br>
 <div>Leider konnten wir Ihr Gesuch nicht berücksichtigen. {{ $d->textblock_denial }}</div>
 <br>
